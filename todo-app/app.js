aaff629ad1e44af0e73cff22ddd20a05cd9bbbef
foolish-pine <matsuda.takuya41@gmail.com>
2022-12-04 20:22:23 +0900

    cp26

---
+++ todo-app/app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const taskRoute = require("./routes/tasks");
+const PORT = 5000;
+
+// ルーティング設計
+app.use("/api/v1/tasks", taskRoute);
+
+app.listen(PORT, console.log("サーバー起動"));
