6e5895e7fd768a741770c02a65116acd7fa254e1
foolish-pine <matsuda.takuya41@gmail.com>
2022-12-29 17:56:42 +0900

    54

--- todo-app/public/script.js
+++ todo-app/public/script.js
@@ -24,7 +24,7 @@ const showTasks = async () => {
 					<span><i class="far fa-check-circle"></i></span>${name}
 				</h5>
 				<div class="task-links">
-					<a href="#" class="edit-link"><i class="fas fa-edit"></i></a>
+					<a href="/edit.html?id=${_id}" class="edit-link"><i class="fas fa-edit"></i></a>
 					<button type="button" class="delete-btn" data-id="${_id}"><i class="fas fa-trash"></i></button>
 				</div>
 			</div>`;
