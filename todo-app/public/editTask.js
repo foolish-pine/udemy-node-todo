746f3ceedf1c0477e89555241b5f4ebca5699e6b
foolish-pine <matsuda.takuya41@gmail.com>
2022-12-29 18:36:26 +0900

    58

--- todo-app/public/editTask.js
+++ todo-app/public/editTask.js
@@ -1,5 +1,8 @@
 const taskIDDOM = document.querySelector(".task-edit-id");
 const taskNameDOM = document.querySelector(".task-edit-name");
+const editFormDOM = document.querySelector(".single-task-form");
+const formAlertDOM = document.querySelector(".form-alert");
+const taskCompletedDOM = document.querySelector(".task-edit-completed");
 
 const params = window.location.search;
 const id = new URLSearchParams(params).get("id");
@@ -10,9 +13,27 @@ const showTask = async () => {
 		const { _id, completed, name } = task;
 		taskIDDOM.textContent = _id;
 		taskNameDOM.value = name;
+		taskCompletedDOM.checked = completed;
 	} catch (err) {
 		console.log(err);
 	}
 };
 
 showTask();
+
+editFormDOM.addEventListener("submit", async (e) => {
+	e.preventDefault();
+	try {
+		const taskName = taskNameDOM.value;
+		const taskCompleted = taskCompletedDOM.checked;
+		const { data: task } = await axios.patch(`/api/v1/tasks/${id}`, {
+			name: taskName,
+			completed: taskCompleted,
+		});
+		formAlertDOM.getElementsByClassName.display = "block";
+		formAlertDOM.textContent = "編集に成功しました";
+		formAlertDOM.classList.add("text-success");
+	} catch (err) {
+		console.log(err);
+	}
+});
